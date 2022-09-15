e85a86b020fe27192d8fc2aee7222050bdc52ba9
Salatchen <pozhivi.s.moe@gmail.com>
2022-09-15 14:22:37 +0300

    initial commit

---
+++ Exercise01_HowManyMoreThanZero/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл
+//  пользователь.
+// 5
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+// вариация задачи 41 - пользователь не задает сколько чисел он введет (то есть число M), 
+// а подсчет количества чисел производится после того, как пользователь не ввел информацию и 
+// нажал Enter.
+
+Console.Write("Please, enter numbers. Use space to split: ");
+
+try
+{
+    string numbers = Console.ReadLine();
+    var nums = numbers.Split(" ").Select(Int32.Parse).ToArray();
+
+    int HowManyOverZero(int[] digits)
+    {
+        int sum = 0;
+        for (int i = 0; i < digits.Length; i++)
+        {
+            if (digits[i] > 0)
+            {
+                sum += 1;
+            }
+        }
+        return sum;
+    }
+
+    Console.WriteLine(HowManyOverZero(nums));
+}
+catch
+{
+    Console.WriteLine("Please, enter integers next time.");
+}
+
+
+
---
+++ Exercise02_CrossPoint/Program.cs
@@ -0,0 +1,48 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных 
+// уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double[] GetCoefficients()
+{
+    double[] coefficients = new double[4];
+    Console.Write("Please, enter value k1: ");
+    coefficients[0] = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write("Please, enter value b1: ");
+    coefficients[1] = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write("Please, enter value k2: ");
+    coefficients[2] = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write("Please, enter value b2: ");
+    coefficients[3] = Convert.ToDouble(Console.ReadLine());
+
+    return coefficients;
+
+}
+
+
+
+double[] FindCrosspoint(double[] coeffs)
+{
+    double[] crosspoint = new double[2];
+    
+    crosspoint[0] = (coeffs[3] - coeffs[1]) / (coeffs[0] - coeffs[2]);
+
+    crosspoint[1] = coeffs[0] * crosspoint[0] + coeffs[1];
+
+    return crosspoint;
+}
+
+void PrintArray(double[] array)
+{
+    Console.Write("Crosspoint coordinates are: [ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine("]");
+}
+
+PrintArray(FindCrosspoint(GetCoefficients()));
+
